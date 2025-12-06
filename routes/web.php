0c682a74cba507499fc9a005d774872d57e6ef4a
Berenice Lopez Gonzalez <berenicelop04@utchetumal.edu.mx>
2025-12-05 19:56:27 -0500

    Agregar ruta de prueba en web.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use App\Models\Contacto;
 use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('formulario_contacto');
+//Route::get('/', function () {
+   // return view('formulario_contacto');
+//});
+Route::get('/prueba', function() {
+    return 'Laravel funciona en Azure!';
 });
+
 
 Route::post('/save', function (Request $request) {
     Contacto::create([
